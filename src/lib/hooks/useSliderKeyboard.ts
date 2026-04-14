f190c40a83f152e98223ce7e048dc1ff194ec87f
Quoc Thanh <qthan1004@github.com>
2026-04-14 22:24:52 +0700

    style(slider): apply ad-hoc Pick for action map parameters

--- src/lib/hooks/useSliderKeyboard.ts
+++ src/lib/hooks/useSliderKeyboard.ts
@@ -1,34 +1,50 @@
 import { clamp } from '../helpers';
-import type { SliderBaseProps } from '../models';
 
-export type UseSliderKeyboardOptions = Required<Pick<SliderBaseProps, 'min' | 'max' | 'step'>> &
-  Pick<SliderBaseProps, 'disabled'>;
-
-export type SliderKeyboardActionOptions = Pick<UseSliderKeyboardOptions, 'min' | 'max' | 'step'> & {
+interface SliderKeyboardActionOptions {
   val: number;
+  step: number;
+  min: number;
+  max: number;
+}
+
+const KEY_ACTIONS = {
+  ArrowLeft: ({ val, step, min, max }: SliderKeyboardActionOptions) =>
+    clamp(val - step, min, max),
+  ArrowDown: ({ val, step, min, max }: SliderKeyboardActionOptions) =>
+    clamp(val - step, min, max),
+  ArrowRight: ({ val, step, min, max }: SliderKeyboardActionOptions) =>
+    clamp(val + step, min, max),
+  ArrowUp: ({ val, step, min, max }: SliderKeyboardActionOptions) =>
+    clamp(val + step, min, max),
+  Home: ({ min }: Pick<SliderKeyboardActionOptions, 'min'>) => min,
+  End: ({ max }: Pick<SliderKeyboardActionOptions, 'max'>) => max,
 };
 
-const KEY_ACTIONS: Record<string, (options: SliderKeyboardActionOptions) => number> = {
-  ArrowLeft: ({ val, step, min, max }) => clamp(val - step, min, max),
-  ArrowDown: ({ val, step, min, max }) => clamp(val - step, min, max),
-  ArrowRight: ({ val, step, min, max }) => clamp(val + step, min, max),
-  ArrowUp: ({ val, step, min, max }) => clamp(val + step, min, max),
-  Home: ({ min }) => min,
-  End: ({ max }) => max,
-};
+interface UseSliderKeyboardOptions {
+  min: number;
+  max: number;
+  step: number;
+  disabled?: boolean;
+}
 
-export function useSliderKeyboard({ min, max, step, disabled }: UseSliderKeyboardOptions) {
-  return (e: React.KeyboardEvent, currentValue: number, onValueChange: (newValue: number) => void) => {
+export function useSliderKeyboard({
+  min,
+  max,
+  step,
+  disabled,
+}: UseSliderKeyboardOptions) {
+  return (
+    e: React.KeyboardEvent,
+    currentValue: number,
+    onValueChange: (newValue: number) => void,
+  ) => {
     if (disabled) return;
 
-    const action = KEY_ACTIONS[e.key];
-    if (action) {
-      e.preventDefault();
-      const newValue = action({ val: currentValue, step, min, max });
+    const action = KEY_ACTIONS?.[e.key as keyof typeof KEY_ACTIONS];
+    if (!action) return;
+    e.preventDefault();
+    const newValue = action({ val: currentValue, step, min, max });
 
-      if (newValue !== currentValue) {
-        onValueChange(newValue);
-      }
-    }
+    if (newValue !== currentValue) onValueChange(newValue);
   };
 }
